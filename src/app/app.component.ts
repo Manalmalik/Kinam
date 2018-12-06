f69338271448891066bb4d1ecfe149c66eab797c
Michael Gerullis <michigerullis92@gmail.com>
2018-12-06 15:08:10 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kinam-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'kinam';
+}
