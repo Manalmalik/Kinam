c6c3febcc59c47d57f811db0949c3ce00f6c5508
Michael Gerullis <michigerullis92@gmail.com>
2019-03-27 23:10:08 +0100

    add parallax

--- projects/website/src/app/routes/main/main.component.ts
+++ projects/website/src/app/routes/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
@@ -12,19 +12,47 @@ export class MainComponent {
   public fullPage;
   public config;
 
-  constructor(private sanitize: DomSanitizer) {
-    this.config = {
-      licenseKey: 'No License key',
-      anchors: ['welcom', 'mission'],
-      sectionsColor: ['#000000'],
-    };
+  @ViewChild('scroll') scroll: ElementRef;
+
+  public get percentOfStory() {
+    const val =
+      (this.scroll.nativeElement.scrollLeft + this.scroll.nativeElement.clientWidth)
+     / this.scroll.nativeElement.scrollWidth;
+    return val;
   }
 
-  public getRef(fullPageRef) {
-    this.fullPage = fullPageRef;
+  public get card2Transform() {
+    return (400 - (this.percentOfStory * 3 * 200) > 0) ? 
+    { 'transform':  `translateY(-${320 - (this.percentOfStory * 3 * 200)}px)`}
+    : null;
   }
 
-  public getSanitized(url: string): SafeResourceUrl {
-    return this.sanitize.bypassSecurityTrustResourceUrl(url);
+  public get card3Transform() {
+    console.log(600 - this.percentOfStory * 3 * 200)
+    return (600 - (this.percentOfStory * 3 * 200) > 0) ? 
+    { 'transform':  `translateY(-${600 - (this.percentOfStory * 3 * 200)}px)`}
+    : null;
   }
+
+  // public get card3Transform() {
+  //   return (this.percentOfStory > 0. && this.percentOfStory < 0.66) ? 
+  //   { 'transform':  `translateY(${400 - this.percentOfStory * 600}px)`}
+  //   : null;
+  // }
+
+  public get scrollLeftCss() { 
+    return { 'background-position-x':  `-${this.scroll.nativeElement.scrollLeft * 1.5}px`};
+  }
+
+  public get sec2Css() {
+    return { }
+  }
+
+  goTo(id)  {
+    document.getElementById(id).scrollIntoView({behavior: 'smooth'})
+  }
+
+  constructor(private sanitize: DomSanitizer) { }
+
+  handleScroll() { }
 }
