b0264e943df6405a3ba2ccd65664be9f96cc68c5
Julius Zehntel <julius.eins.null@gmail.com>
2019-07-05 09:56:54 +0200

    add kaqchikel names

--- projects/nahual-date/src/lib/day-sign.ts
+++ projects/nahual-date/src/lib/day-sign.ts
@@ -1,102 +1,122 @@
 export const DAY_SIGNS = [
   {
     name: 'imix',
-    label: 'Imix',
+    'label-yucatec': 'Imix',
+    'label': 'Imox',
     day: 1
   },
   {
     name: 'ik',
-    label: 'Ikʼ',
+    'label-yucatec': 'Ikʼ',
+    'label': 'Iqʼ',
     day: 2
   },
   {
     name: 'akbal',
-    label: 'Akʼbʼal',
+    'label-yucatec': 'Akʼbʼal',
+    'label': 'Aqʼabʼal',
     day: 3
   },
   {
     name: 'kan',
-    label: 'Kʼan',
+    'label-yucatec': 'Kʼan',
+    'label': 'Kʼat',
     day: 4
   },
   {
     name: 'chicchan',
-    label: 'Chikchan',
+    'label-yucatec': 'Chikchan',
+    'label': 'Kan',
     day: 5
   },
   {
     name: 'cimi',
-    label: 'Kimi',
+    'label-yucatec': 'Kimi',
+    'label': 'Kame',
     day: 6
   },
   {
     name: 'manik',
-    label: 'Manikʼ',
+    'label-yucatec': 'Manikʼ',
+    'label': 'Kej',
     day: 7
   },
   {
     name: 'lamat',
-    label: 'Lamat',
+    'label-yucatec': 'Lamat',
+    'label': 'Qʼanil',
     day: 8
   },
   {
     name: 'muluc',
-    label: 'Muluk',
+    'label-yucatec': 'Muluk',
+    'label': 'Toj',
     day: 9
   },
   {
     name: 'oc',
-    label: 'Ok',
+    'label-yucatec': 'Ok',
+    'label': 'Tʼzi',
     day: 10
   },
   {
     name: 'chuen',
-    label: 'Chuwen',
+    'label-yucatec': 'Chuwen',
+    'label': `B'atz'`,
     day: 11
   },
   {
     name: 'eb',
-    label: 'Ebʼ',
+    'label-yucatec': 'Ebʼ',
+    'label': `E`,
     day: 12
   },
   {
     name: 'ben',
-    label: 'Bʼen',
+    'label-yucatec': 'Bʼen',
+    'label': 'Aj',
     day: 13
   },
   {
     name: 'ix',
-    label: 'Ix',
+    'label-yucatec': 'Ix',
+    'label': `I'x`,
     day: 14
   },
   {
     name: 'men',
-    label: 'Men',
+    'label-yucatec': 'Men',
+    'label': `Tz'ikin`,
     day: 15
   },
   {
     name: 'cib',
-    label: 'Kibʼ',
+    'label-yucatec': 'Kibʼ',
+    'label': 'Ajmaq',
     day: 16
   },
   {
     name: 'caban',
-    label: 'Kabʼan',
+    'label-yucatec': 'Kabʼan',
+    'label': `No'j`,
     day: 17
   },
   {
     name: 'etznab',
-    label: 'Etzʼnabʼ',
+    'label-yucatec': 'Etzʼnabʼ',
+    'label': 'Tijaxʼ',
     day: 18
   },
   {
     name: 'cauac',
-    label: 'Kawak',
+    'label-yucatec': 'Kawak',
+    'label': 'Kawoq',
     day: 19
   },
   {
     name: 'ahau',
-    label: 'Ajaw',
+    'label-yucatec': 'Ajaw',
+    'label': 'Ajpu',
     day: 20
   }
 ];
