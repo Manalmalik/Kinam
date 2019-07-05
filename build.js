98ff83be5bd8ba0259363f55f2f053a70f49f6d7
Michael Gerullis <morefox@Michaels-MacBook-Pro.local>
2019-07-05 14:13:55 +0200

    update responsiveness

--- build.js
+++ build.js
@@ -4,18 +4,17 @@ const base = new Promise(resolve => {
   child.exec('npm run -- ng build nahual-date', () => {
     console.log('nahual-date done');
   });
+
   child.exec('npm run -- ng build shared', () => {
     console.log('shared done');
   });
+  
   child.exec('npm run -- ng build core', () => {
     console.log('core done');
-
     child.exec('npm run -- ng build ng-maya-number', () => {
       console.log('maya-number done');
-      resolve();
       child.exec('npm run -- ng build ng-maya-birthday', () => {
         console.log('birth-date done');
-
         child.exec('npm run -- ng build website --prod', () => {
           resolve();
           console.log('all done');
