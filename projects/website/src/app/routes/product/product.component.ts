0486603c17873d2e49476e09541c7495aa57513e
Julius Zehntel <julius.eins.null@gmail.com>
2019-07-19 11:47:45 +0200

    update styling

--- projects/website/src/app/routes/product/product.component.ts
+++ projects/website/src/app/routes/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { AbstractMenu } from '@website/components/menu/abstract-menu';
-import { LandingService } from '@website/services';
+import { LandingService, TitleService } from '@website/services';
 import { AnimationBuilder, style, animate } from '@angular/animations';
 
 @Component({
@@ -11,11 +11,16 @@ import { AnimationBuilder, style, animate } from '@angular/animations';
 export class ProductComponent extends AbstractMenu implements OnInit {
   @ViewChild('main') main: ElementRef;
 
-  constructor(landingService: LandingService, private _builder: AnimationBuilder) {
+  constructor(landingService: LandingService, private _builder: AnimationBuilder, private titleService: TitleService) {
     super(landingService);
   }
 
   public ngOnInit() {
+
+    this.titleService.setTitle({
+      description: 'The Jacket - Fashion. Music. Art. Technology. - Support our Crowdfunding!'
+    });
+
     const anim = this._builder.build([
       style({
         transform: `translateY(-100px)`,
